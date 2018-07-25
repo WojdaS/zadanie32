faf316198aaf38e5d2c4637808a9329e7a66d093
Slawomir Wojda <slwojda@poczta.onet.pl>
2018-07-25 21:31:26 +0200

    popównywanie dwoch liczb

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var num1 = 5;
+var num2 = 5;
+
+
+function porownaj(parametr1, parametr2) {
+    if (parametr1 > parametr2) {
+        console.log(parametr1);
+    } else {
+        console.log(parametr2);
+    }
+}
+
+porownaj(num1, num2);
